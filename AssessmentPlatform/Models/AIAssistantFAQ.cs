e27193a58b1a8d841c08afc0883e9fa5f7fb8dcc
paramsingh <parampreet.digisoft@gmail.com>
2026-05-12 15:21:05 +0530

    Commit for addition of new column in AIAssistantFAQ

--- AssessmentPlatform/Models/AIAssistantFAQ.cs
+++ AssessmentPlatform/Models/AIAssistantFAQ.cs
@@ -10,5 +10,7 @@
         public bool IsActive { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public string ProcName { get; set; }
     }
 }
